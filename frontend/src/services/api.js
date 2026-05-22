5aee616321bb5e31eb9b93518ca99db71d55c187
anishwakvlns04 <125719729+anishwakvlns04@users.noreply.github.com>
2026-05-22 14:15:08 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080"
+  baseURL: "https://blissful-nature-production-7368.up.railway.app"
 });
 
 export default API;
